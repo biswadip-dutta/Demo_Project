0a0c1ecf6562214959009b17b75fbef5898844fe
Biswadip Dutta <developertectes@gmail.com>
2023-02-22 19:48:12 +0530

    To test

--- src/main/java/org/example/ParentItem.java
+++ src/main/java/org/example/ParentItem.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParentItem extends Item{
+public class ParentItem extends Item {
     List<Item> children = new ArrayList<>();
 
     public ParentItem(String name, BigDecimal value, boolean calculated) {
